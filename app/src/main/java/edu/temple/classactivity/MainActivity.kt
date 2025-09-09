37c499ab9125a9613c0b0d3b30529f947b4e3cfe
Mohammed Karim <makarim913@temple.edu>
2025-09-09 12:23:01 -0400

    class activity done

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,14 +1,38 @@
 package edu.temple.classactivity
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val myButton = findViewById<Button>(R.id.clickmeButton)
+
+        val displayText = findViewById<TextView>(R.id.textView)
+
+        val nameEditText = findViewById<EditText>(R.id.nameEditText)
+
+
+
+        findViewById<Button>(R.id.clickmeButton).setOnClickListener {
+            if (nameEditText.text.isNotEmpty()) {
+                displayText.text = "Hello, ${nameEditText.text}!"
+            } else {
+                displayText.text = "Please enter your name"
+            }
+            nameEditText.text.clear()
+        }
+
+
     }
 }
+
